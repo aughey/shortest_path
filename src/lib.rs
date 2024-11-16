2b674a4f1f651a7e70085adc4a89f6669808b0ff
John Aughey <jha@aughey.com>
2024-11-16 15:49:00 +0000

    youtube red

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+fn min_path_sum_youtube(grid: &[&[i32]]) -> i32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
 
-    const PROBLEM: &[&[u8]] = &[
+    const PROBLEM: &[&[i32]] = &[
         &[1,3,1],
         &[1,5,1],
         &[4,2,1]
@@ -13,5 +17,10 @@ mod tests {
         for row in PROBLEM {
             assert_eq!(row.len(), 3);
         }
+    }
+
+    #[test]
+    fn test_min_path_sum_youtube() {
+        assert_eq!(super::min_path_sum_youtube(PROBLEM), 7);
     }
 }
